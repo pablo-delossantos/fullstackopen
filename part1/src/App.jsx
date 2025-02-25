79c7ac0a5950314d7ea4a24ffc92109e71211bc8
Pablo Ariel De Los Santos <pdelossantos@nosis.com>
2025-02-25 17:11:43 -0300

    Ejercicio 0

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,29 +1,37 @@
-const Hello = (props) => {
+const Header = ({ course }) => {
+  return <h1>{course}</h1>;
+};
 
-  console.log(props)
+const Content = ({ parts }) => {
   return (
     <div>
-      <p>
-
-        Hello {props.name}, you are {props.age} years old
-      </p>
+      <p>{parts[0].name} {parts[0].exercises}</p>
+      <p>{parts[1].name} {parts[1].exercises}</p>
+      <p>{parts[2].name} {parts[2].exercises}</p>
     </div>
-  )
-}
+  );
+};
+
+const Total = ({ parts }) => {
+  const totalExercises = parts[0].exercises + parts[1].exercises + parts[2].exercises;
+  return <p>Number of exercises {totalExercises}</p>;
+};
 
 const App = () => {
-
-  const name = 'Peter'
-  const age = 10
+  const course = "Half Stack application development";
+  const parts = [
+    { name: "Fundamentals of React", exercises: 10 },
+    { name: "Using props to pass data", exercises: 7 },
+    { name: "State of a component", exercises: 14 },
+  ];
 
   return (
     <div>
-      <h1>Greetings</h1>
-
-      <Hello name='Maya' age={26 + 10} />
-      <Hello name={name} age={age} />
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
-  )
-}
+  );
+};
 
 export default App;
