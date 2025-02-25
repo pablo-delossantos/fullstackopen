f6721658183482f1a67e4db2c350974d367cbc71
Pablo Ariel De Los Santos <pdelossantos@nosis.com>
2025-02-25 14:14:37 -0300

    Eslint config

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,9 +1,29 @@
-const App = () => {
+const Hello = (props) => {
+
+  console.log(props)
   return (
     <div>
-      <p>Hello world</p>
+      <p>
+
+        Hello {props.name}, you are {props.age} years old
+      </p>
     </div>
   )
 }
 
-export default App
+const App = () => {
+
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  )
+}
+
+export default App;
